9eafcc8f459e39cd485dc07d6a10974cc0153cac
MarcosHBlanco <124410355+MarcosHBlanco@users.noreply.github.com>
2026-03-08 11:50:11 -0700

    feat:implement movies API endpoints

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.get("/about", (req, res) => {
 	res.send("This is my movie tracker backend!");
 });
 
+app.get("/movies", (req, res) => {
+	const movies = [
+		{ id: 1, title: "Inception", year: 2010 },
+		{ id: 2, title: "Interestellar", year: 2014 },
+	];
+
+	res.json(movies);
+});
+
+app.post("/movies", (req, res) => {
+	res.json({
+		message: "Movie added succesfully",
+	});
+});
+
 app.listen(PORT, () => {
 	console.log(`Server runnin on port ${PORT}`);
 });
